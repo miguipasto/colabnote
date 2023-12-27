693239c60ed368eea4438d4a7160b802ab7abc3d
Miguel Pastoriza - UbuntuServer <you@example.com>
2023-12-27 20:45:02 +0000

    Share noted working

--- BackEnd/src/controllers/notesController.js
+++ BackEnd/src/controllers/notesController.js
@@ -73,13 +73,28 @@ export const shareNote = async (req, res) => {
     const db_address = db.address.toString();
     console.log(`Base de datos creada: ${db_address}`);
 
-    // Añadimos el documento a la base de datos
-    const newNote = { _id: noteId, title: couchbaseContent.title, content: couchbaseContent.content };
-    const hash = await db.put(newNote);
+    // Esperamos la conexion
+    db.events.on('replicate', () => {
+      console.log('Esperando conexiones');
+    });
+    
+    db.events.on('replicated', () => {
+      console.log('Nueva conexion');
+      const datos = db.get('');
+      console.log(datos[0])
+      // Añadimos el documento a la base de datos
+      const newNote = { _id: noteId, title: couchbaseContent.title, content: couchbaseContent.content };
+      db.put(newNote);
 
-    console.log(`Nuevo dato publicado en la base de datos: ${hash}`);
+      console.log(`Nuevo dato publicado en la base de datos: ${db_address}`);
+    });
 
-    db.close()
+    db.events.on('write', (address, entry) => {
+      console.log(`Nuevo dato escrito: ${address}`);
+      console.log(entry.payload.value);
+    });
+
+ 
 
     res.json({ orbitDB_database: db_address });
   } catch (error) {
@@ -88,6 +103,44 @@ export const shareNote = async (req, res) => {
   }
 };
 
+// export const shareNote = async (req, res) => {
+//   try {
+//     const noteId = req.params.id;
+
+//      // Conectar a Couchbase
+//      const collection = await couchBaseCollection();
+
+//      // Obtener y mostrar el contenido del documento de Couchbase
+//      const result = await collection.get(noteId);
+//      const couchbaseContent = result.content;
+
+//     console.log(couchbaseContent)
+
+//     //Configuramos las opciones de la base de datos
+//     const optionsToWrite = {
+//       accessController: {
+//         type: 'orbitdb',
+//         write: ['*'],
+//       }
+//     }
+
+//     // Creamos la base de datos
+//     const orbitdb = await orbitDBInstance();
+//     const db = await orbitdb.docs(noteId, optionsToWrite); 
+//     await db.load();
+
+//     const db_address = db.address.toString();
+//     console.log(`Base de datos creada: ${db_address}`);
+
+//     const newNote = { _id: noteId, title: couchbaseContent.title, content: "otro" };
+//     await db.put(newNote);
+
+//     res.json({ orbitDB_database: db_address });
+//   } catch (error) {
+//     console.error('Error al compartir la nota en OrbitDB:', error);
+//     res.status(500).json({ error: 'Internal Server Error' });
+//   }
+// };
 
 
 export const getSharedNote = async (req, res) => {
@@ -97,29 +150,66 @@ export const getSharedNote = async (req, res) => {
 
     // Nos conectamos a la base de datos OrbitdB
     const orbitdb = await orbitDBInstance();
-    const db = await orbitdb.open(dbAddress);
+    const db = await orbitdb.open(dbAddress + "/" + noteId);
     await db.load();
     console.log(`Conectado a la base de datos: ${db.address.toString()}`);
-    
-    let datos =""
-    datos = await db.get('');
 
-    // Replicamos los valores que hay en ella
+    // Indicamos que estamos conectados
+    const newNote = { _id: noteId, title: "Connected", content: "From Node 2" };
+    await db.put(newNote);
+
+    // Esperamos los datos
+    let datos = ""
     db.events.on('replicate', () => {
       console.log('Replicando cambios...');
     });
     
-    await db.events.on('replicated', () => {
+    db.events.on('replicated', () => {
       console.log('Cambios replicados con éxito.');
       datos = db.get('');
       console.log(datos)
+      res.json({ orbitDB_database: datos });    
     });
 
-    db.close()
-
-    res.json({ orbitDB_database: datos });
   } catch (error) {
     console.error('Error al compartir la nota en OrbitDB:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 };
+
+
+// export const getSharedNote = async (req, res) => {
+//   try {
+//     const dbAddress = req.params.dbAddress;
+//     const noteId = req.params.noteId;
+
+//     // Nos conectamos a la base de datos OrbitdB
+//     const orbitdb = await orbitDBInstance();
+//     const db = await orbitdb.open(dbAddress + "/" + noteId);
+//     await db.load();
+//     console.log(`Conectado a la base de datos: ${db.address.toString()}`);
+
+//     const datos = db.get('')
+
+//     // // Replicamos los valores que hay en ella
+//     // db.events.on('replicate', () => {
+//     //   console.log('Replicando cambios...');
+//     // });
+    
+//     // await db.events.on('replicated', () => {
+//     //   console.log('Cambios replicados con éxito.');
+//     //   const datos = db.get('');
+//     //   console.log(datos)
+//     // });
+
+//     // db.events.on('write', (address, entry) => {
+//     //   console.log(`Nuevo dato escrito: ${address}`);
+//     //   console.log(entry.payload.value);
+//     // });
+    
+//     res.json({ orbitDB_database: datos });    
+//   } catch (error) {
+//     console.error('Error al compartir la nota en OrbitDB:', error);
+//     res.status(500).json({ error: 'Internal Server Error' });
+//   }
+// };
